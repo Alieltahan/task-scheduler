5c0df83b9690ac5b0e280ae3b701e4e8aed7c628
Alieltahan <alyaltahan@gmail.com>
2022-09-06 11:15:39 +0200

    Wrapped Styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { AuthContext, AuthProvider } from './components/Auth/AuthProvider';
+import Home from './components/Home/Home';
+import Navbar from './components/Navbar/Navbar';
+import LandingPage from './pages/landingPage';
+import SigninPage from './pages/signinPage';
+import SignupPage from './pages/signupPage';
+import NotFoundPage from './pages/notFoundPage';
+import { useContext } from 'react';
 
 function App() {
+  const currentUser = useContext(AuthContext);
+  console.log(currentUser);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AuthProvider>
+        <Navbar />
+        <Routes>
+          <Route
+            path="home"
+            element={currentUser ? <Home /> : <Navigate to="/signin" replace />}
+          />
+          <Route path="/signin" element={<SigninPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </AuthProvider>
     </div>
   );
 }
