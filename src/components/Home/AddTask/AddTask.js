eb664db17b3f102eb5b17517e06e0f94ed578590
Alieltahan <alyaltahan@gmail.com>
2022-09-11 19:58:07 +0200

    Show / Hide Add Task Form

--- src/components/Home/AddTask/AddTask.js
+++ src/components/Home/AddTask/AddTask.js
@@ -7,6 +7,7 @@ import useForm from '../../lib/useForm';
 
 const AddTask = () => {
   const [submitting, setSubmitting] = useState(false);
+  const [showForm, setShowForm] = useState(false);
   const { handleChange, inputs, resetForm } = useForm({
     title: '',
     description: '',
@@ -36,54 +37,75 @@ const AddTask = () => {
   };
 
   return (
-    <form className="form" onSubmit={handleSubmit} method="POST">
-      <fieldset disabled={submitting} aria-busy={submitting}>
-        <Input
-          label="Title"
-          name="title"
-          value={inputs.title}
-          onChange={handleChange}
-          type="text"
-          required={true}
-        />
-
-        <Input
-          label="Description"
-          name="description"
-          value={inputs.description}
-          onChange={handleChange}
-          type="text"
-          required={false}
-        />
-        <div className="form__group form__select">
-          <select
-            onChange={handleChange}
-            className="form__input"
-            name="priority"
-            id="priority"
-          >
-            <option value="High">High</option>
-            <option value="Medium">Medium</option>
-            <option value="Low">Low</option>
-          </select>
-          <label className="form__label" htmlFor="priority">
-            Please select priority:
-          </label>
-        </div>
-
-        <Input
-          label="Due Date"
-          name="dueDate"
-          value={inputs.dueDate}
-          onChange={handleChange}
-          type="date"
-          required={true}
-        />
-        <button type="submit" value="submit" className="form__btn">
-          Add Task
+    <>
+      {!showForm && (
+        <button
+          type="button"
+          onClick={() => setShowForm(true)}
+          className="form__btn"
+        >
+          Add New Task
         </button>
-      </fieldset>
-    </form>
+      )}
+      {showForm && (
+        <form className="form" onSubmit={handleSubmit} method="POST">
+          <fieldset disabled={submitting} aria-busy={submitting}>
+            <Input
+              label="Title"
+              name="title"
+              value={inputs.title}
+              onChange={handleChange}
+              type="text"
+              required={true}
+            />
+
+            <Input
+              label="Description"
+              name="description"
+              value={inputs.description}
+              onChange={handleChange}
+              type="text"
+              required={false}
+            />
+            <div className="form__group form__select">
+              <select
+                onChange={handleChange}
+                className="form__input"
+                name="priority"
+                id="priority"
+              >
+                <option value="High">High</option>
+                <option value="Medium">Medium</option>
+                <option value="Low">Low</option>
+              </select>
+              <label className="form__label" htmlFor="priority">
+                Please select priority:
+              </label>
+            </div>
+
+            <Input
+              label="Due Date"
+              name="dueDate"
+              value={inputs.dueDate}
+              onChange={handleChange}
+              type="date"
+              required={true}
+            />
+            <button type="submit" value="submit" className="form__btn">
+              Add Task
+            </button>
+            <button
+              onClick={() => setShowForm(false)}
+              type="submit"
+              value="cancel"
+              className="form__btn"
+            >
+              Cancel
+            </button>
+          </fieldset>
+        </form>
+      )}
+    </>
   );
 };
 
