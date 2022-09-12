2fe514559de4d7dcb822fdabc40931b7b336b40e
Alieltahan <alyaltahan@gmail.com>
2022-09-12 23:01:50 +0200

    Update time formatting Fn

--- src/components/lib/timeFormating.js
+++ src/components/lib/timeFormating.js
@@ -1,5 +1,9 @@
 const dateToString = (time) => {
-  return new Date(time).toDateString();
+  return new Date(time).toLocaleString('default', {
+    month: 'short',
+    year: 'numeric',
+    day: 'numeric',
+  });
 };
 
 const dateToNumber = (time) => {
